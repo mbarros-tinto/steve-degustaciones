97f2ef0a37c8009708e2a72d905b7b20e90d6b1a
mbarros-pronect <mbarros@pronect.com>
2026-04-16 11:17:22 -0400

    Set API_URL to live Apps Script deployment
    Points to v103 deployment with API.gs router enabled.

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // URL del Apps Script desplegado como Web App.
 // Actualizar después de hacer el deployment con API.gs incluido.
-window.API_URL = "PLACEHOLDER_APPS_SCRIPT_URL";
+window.API_URL = "https://script.google.com/macros/s/AKfycbz1bVXgkUS9FC5AL87ybZNmsnx479I8Ie9TMfxw57YQZn8HAnh8CXNroGBxI7NDQW7Q/exec";
